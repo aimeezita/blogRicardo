80dbf303db5df041800fc1fb13e3b3009a73751d
aimeezita <rosettethompson@gmail.com>
2023-06-02 12:52:35 -0300

    mudanças na usuarioRepository

--- src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package com.generation.blogpessoal.repository;
 
+
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,5 +9,5 @@ import com.generation.blogpessoal.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
-	public Optional<Usuario> findByUsuario(String usuario);
+	Optional<Usuario> findByUsuario(String usuario);
 }
