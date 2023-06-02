d0d826fa78047667b9fc5d5f746c86b264482396
aimeezita <rosettethompson@gmail.com>
2023-06-02 14:37:51 -0300

    postagem repository

--- src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
-
 import com.generation.blogpessoal.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem, Long>{
 
-	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+	List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
+
 }
